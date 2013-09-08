66db2c0a93f7afea78c0bf95d08b31cf91b68df5
Joseph Hart <josephrhart08@yahoo.com>
2013-09-07 22:45:47 -0500

    Added GUI template and a very basic title screen
    Various scripts included. GUI still needs custom textures

---
+++ newgame/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraShake : MonoBehaviour
+{
+    
+    private Vector3 originPosition;
+    private Quaternion originRotation;
+    public float shakeDecay;
+    public float shakeIntensity;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Shake"))
+        {
+            //Mathf.PerlinNoise(
+            Shake();
+        }
+    }
+
+    void Update()
+    {
+        if (shakeIntensity > 0)
+        {
+            transform.position = originPosition + Random.insideUnitSphere * shakeIntensity;
+            transform.rotation = new Quaternion(
+                originRotation.x + Random.Range(-shakeIntensity, shakeIntensity) * .05f,
+                originRotation.y + Random.Range(-shakeIntensity, shakeIntensity) * .05f,
+                originRotation.z + Random.Range(-shakeIntensity, shakeIntensity) * .05f,
+                originRotation.w + Random.Range(-shakeIntensity, shakeIntensity) * .05f);
+            shakeIntensity -= shakeDecay;
+        }
+    }
+
+    void Shake()
+    {
+        originPosition = transform.position;
+        originRotation = transform.rotation;
+        shakeIntensity = .5f;
+        shakeDecay = 0.005f;
+    }
+}
---
+++ newgame/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour 
+{
+    public GameObject player;
+	// Use this for initialization
+	void Start () 
+    {
+        for (float x = -10f; x <= 90f; x += 10f)
+        {
+            GameObject marker = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            marker.renderer.material.color = Color.blue;
+            marker.transform.localScale = new Vector3(1f, 3f, 1f);
+            marker.transform.position = new Vector3(x, 1f, 1.5f);
+        }
+        Debug.Log("Playing audio");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+    void OnGUI()
+    {
+ 
+    }
+}
