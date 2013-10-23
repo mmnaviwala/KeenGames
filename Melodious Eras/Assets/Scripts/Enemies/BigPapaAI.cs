3dedc0440cec4d38e6634c20190d1e35ee379248
jrhart08 <josephrhart08@yahoo.com>
2013-10-23 18:45:39 -0500

    working on miniboss

--- Melodious Eras/Assets/Scripts/Enemies/BigPapaAI.cs
+++ Melodious Eras/Assets/Scripts/Enemies/BigPapaAI.cs
@@ -5,17 +5,33 @@ public class BigPapaAI : MonoBehaviour
 {
     Transform eye;
     private int hp = 3;
+    public bool jumping = true;
+    public float speed = 7;
 	// Use this for initialization
 	void Start () 
     {
         eye = this.transform.FindChild("Diskmen1:Giant_Disk_Droup").FindChild("Diskmen1:Giant_Eye");
+        jumping = true;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
         //keeping eye orientation stable
-        eye.rotation = new Quaternion(0.0f, -0.7f, 0.7f, 0.0f);
+        //eye.rotation = new Quaternion(0.0f, -0.7f, 0.7f, 0.0f);
+
+        //snapping the character down to the ground for smooth descent
+        if (!jumping)
+        {
+            //this.rigidbody.useGravity = false;
+            RaycastHit hit;
+            this.transform.position = new Vector3(transform.position.x + (speed * Time.deltaTime), transform.position.y, transform.position.z);
+            Physics.Raycast(this.transform.position, Vector3.down, out hit, 0.25f);
+            if (hit.point != Vector3.zero)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, hit.point.y, this.transform.position.z);
+            }
+        }
         if (hp <= 0)
             Destroy(this.gameObject);
 	}
@@ -23,6 +39,7 @@ public class BigPapaAI : MonoBehaviour
     void OnCollisionEnter(Collision col)
     {
         Debug.Log("Big Papa Collision with " + col.gameObject.tag);
+        jumping = false;
         if (col.gameObject.tag == Tags.ENEMY)
         {
             Destroy(col.gameObject);
---
+++ Melodious Eras/Assets/Scripts/Environment/ObstacleScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleScript : MonoBehaviour 
+{
+    public float timeDelay = 0f;
+    bool falling = false;
+    float velocity = 0f;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (falling)
+        {
+            velocity -= Time.deltaTime * 9.81f;
+            this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + Vector3.down, velocity * Time.deltaTime);
+        }
+	}
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(timeDelay);
+        falling = true;
+    }
+}
