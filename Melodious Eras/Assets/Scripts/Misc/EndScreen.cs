425c3733a8bafe7ae785eefe43312f3ab973d82a
Zachary Kaspar <ztkaspar@outlook.com>
2013-10-23 20:44:08 -0500

    Fix end screen display and added exit button

--- Melodious Eras/Assets/Scripts/Misc/EndScreen.cs
+++ Melodious Eras/Assets/Scripts/Misc/EndScreen.cs
@@ -16,8 +16,6 @@ public class EndScreen : MonoBehaviour {
     {
         music = Camera.main.audio;
         playerHUD = GameObject.FindGameObjectWithTag(Tags.PLAYER).GetComponent<HUD>();
-        endScreen = Resources.Load("End") as Texture2D;
-        btnTexture = Resources.Load("white_dot") as Texture2D;
         playerStats = GameObject.FindGameObjectWithTag(Tags.PLAYER).GetComponent<CharacterStats>();
     }
 
@@ -74,6 +72,12 @@ public class EndScreen : MonoBehaviour {
             GUI.Label(new Rect(Screen.width / 2 - Screen.width / 25, Screen.height - Screen.height * 0.345F, buttonWidth, buttonHeight), playerStats.notes.ToString(), buttonStyle);
 
             GUI.Label(new Rect(Screen.width / 2 - Screen.width / 25, Screen.height - Screen.height * 0.27F, buttonWidth, buttonHeight), totalNotes.ToString(), buttonStyle);
+
+            if (GUI.Button(new Rect(Screen.width * .8f, Screen.height * .8f, Screen.width * 0.15F, Screen.height * 0.075F), "Exit Game", buttonStyle))
+            {
+                Debug.Log("Clicked the exit button");
+                Application.Quit();
+            }
         }
     }
 
