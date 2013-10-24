f6fad6ad8a63f97868975f4ce6782123608852a8
Joseph Hart <josephrhart08@yahoo.com>
2013-10-23 20:48:16 -0500

    3rd person camera; C Sharp apparently suffers from chimerism

--- Melodious Eras/Assets/Scripts/Camera/CameraMovement3D.cs
+++ Melodious Eras/Assets/Scripts/Camera/CameraMovement3D.cs
@@ -10,17 +10,27 @@ public class CameraMovement3D : MonoBehaviour
 	void Start () 
     {
         player = GameObject.FindGameObjectWithTag(Tags.PLAYER).transform;
-        
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
+        SmoothLook();
+        this.transform.position = Vector3.Lerp( this.transform.position, 
+                                                player.transform.position + player.transform.forward * offsetZ + player.transform.right * offsetX + player.transform.up * offsetY , 
+                                                2 * Time.deltaTime);
 	}
 
     public void SetOffset(Vector3 newOffset)
     {
-
+        offsetX = newOffset.x;
+        offsetY = newOffset.y;
+        offsetZ = newOffset.z;
+    }
+    void SmoothLook()
+    {
+        Vector3 relPlayerPosition = player.transform.position + player.transform.forward * 10 + player.transform.up * offsetY - this.transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(relPlayerPosition, Vector3.up);
+        transform.rotation = Quaternion.Lerp(this.transform.rotation, lookRotation, 10 * Time.deltaTime);
     }
 }
