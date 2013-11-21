190b1f244ef6f60e3c555877e0251e0de57b4c97
Muhammad Naviwala <majidnaviwala@gmail.com>
2013-11-21 09:48:10 -0600

    added water pro and script for rotating object

---
+++ Melodious Eras/Assets/Scripts/testCircleMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testCircleMovement : MonoBehaviour {
+
+	public float RotationSpeed = 200f;
+	public Vector3 axis = new Vector3(0, 1, 0);
+	protected GameObject rotateRespectiveTo;
+	
+	void Start () {
+		rotateRespectiveTo = GameObject.FindGameObjectWithTag("Enemy");
+	}
+	
+	void Update () {
+		transform.RotateAround (rotateRespectiveTo.transform.position, axis, RotationSpeed* Time.deltaTime);
+	}
+}
