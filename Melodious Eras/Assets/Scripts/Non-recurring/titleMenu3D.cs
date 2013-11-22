daa2550bc72f7c74f9dfb4f7eee6eaac7e176ce6
Joseph Hart <josephrhart08@yahoo.com>
2013-11-22 00:46:02 -0600

    Changed disk to rotate in just one direction

--- Melodious Eras/Assets/Scripts/Non-recurring/titleMenu3D.cs
+++ Melodious Eras/Assets/Scripts/Non-recurring/titleMenu3D.cs
@@ -5,12 +5,15 @@ public class titleMenu3D : MonoBehaviour
 {
     public GameObject objectType;
     GameObject[] selections;
+    Vector3 localForward;
+    public float rotationSpeed = 5;
 
     float radius = 2;
     public float centerPosY = -10;
 	// Use this for initialization
 	void Start () 
     {
+        localForward = this.transform.forward;
         selections = new GameObject[8];
         int counter = 0;
         for (int angle = 0; angle < 360; angle += 45)
@@ -35,7 +38,8 @@ public class titleMenu3D : MonoBehaviour
             selections[counter].transform.localScale = new Vector3(2, 1, .1f);
             selections[counter].renderer.material.color = new Color((angle + 45) / 360f, (angle + 45) / 360f, (angle + 45) / 360f);
             selections[counter].name = "Cube" + (angle / 45);
-            selections[counter].transform.Rotate(Vector3.forward, 90 - angle);
+            //selections[counter].transform.Rotate(this.transform.forward, 90 - angle);
+            selections[counter].transform.localEulerAngles = new Vector3(0, 0, 90 - angle);
             counter++;
         }
 	}
@@ -51,8 +55,11 @@ public class titleMenu3D : MonoBehaviour
         {
             this.transform.position = new Vector3(this.transform.position.x, .75f, -2);
         }
-        this.transform.Rotate(Vector3.right, (Mathf.PerlinNoise(Time.time / 10, 0.0f) - .5f) * .25f);
-        this.transform.Rotate(Vector3.forward, (Mathf.PerlinNoise(Time.time / 10, 0.0f) - .5f) * .5f);
+        //this.transform.Rotate(Vector3.right, (Mathf.PerlinNoise(Time.time / 10, 0.0f) - .5f) * .25f);
+        //this.transform.Rotate(localForward, (Mathf.PerlinNoise(Time.time / 10, 0.0f) - .5f) * .5f, Space.Self);
+        //this.transform.eulerAngles = new Vector3(0, 30, (Mathf.PerlinNoise(Time.time / 10, 0.0f) - .5f) * 50);
+        //this.transform.Rotate(localForward, 90 * Time.deltaTime);
+        this.transform.eulerAngles = new Vector3(0, 30, this.transform.eulerAngles.z - rotationSpeed * Time.deltaTime);
 	}
 
 
