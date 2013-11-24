8b736f4b52ba21338076df59e85ab562522d9c7e
Muhammad Naviwala <majidnaviwala@gmail.com>
2013-11-24 03:13:43 -0600

    Volcano scene progress. Once player reaches the volcano, the camera perspective changes and the player spirals up and once it reaches the top, he is kind of jumps out and the camera perspective changes to normal 2D again

---
+++ Melodious Eras/Assets/ChangePlayerPositionInVolcano.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePlayerPositionInVolcano : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) 
+	{
+		Vector3 playerPosition = GameObject.Find("Capsule to rotate around").transform.position;
+		playerPosition.y += 4;
+		playerPosition.z += 2;
+		GameObject.Find("Dr C Sharp").transform.position = playerPosition;
+
+		GameObject.Find("Dr C Sharp").GetComponent<testCircleMovement>().enabled = true;
+		GameObject.Find("Dr C Sharp").GetComponent<Rigidbody>().useGravity = false;
+		//GameObject.Find("Dr C Sharp").GetComponent<testCircleMovement>().yOffset = 0.09f;
+	}
+}
