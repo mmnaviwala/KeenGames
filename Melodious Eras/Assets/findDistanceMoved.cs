c616734241c157a306aeb22e00f1874971025a3f
Muhammad Naviwala <majidnaviwala@gmail.com>
2013-11-26 11:35:27 -0600

    Script to calculate the distance when the sphere moves

---
+++ Melodious Eras/Assets/findDistanceMoved.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class findDistanceMoved : MonoBehaviour {
+
+	public float distanceTravelled = 0;
+	Vector3 lastPosition;
+	
+	void Start()
+	{
+		lastPosition = transform.position;
+	}
+	
+	void Update()
+	{
+		distanceTravelled += Vector3.Distance(transform.position, lastPosition);
+		lastPosition = transform.position;
+	}
+}
