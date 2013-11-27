3f8f0abd3606954bf9990c4d59d70c5cc5e1475e
Joseph Hart <josephrhart08@yahoo.com>
2013-11-26 22:48:07 -0600

    Changes for the night pt 2

---
+++ Melodious Eras/Assets/Scripts/Environment/Environment.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Environment
+{
+    public static Vector3 wind; //wind direction/speed
+    public static float turbulence; //variation in wind speed
+
+    private static float magX, magY, magZ; //for wind.
+
+
+    public static void SetWind(Vector3 windDirection)
+    {
+        wind = windDirection;
+        magX = wind.x;
+        magY = wind.y;
+        magZ = wind.z;
+    }
+
+    static IEnumerator BlowWind()
+    {
+        while (!wind.Equals(Vector3.zero))
+        {
+            wind = new Vector3(magX + Random.Range(-turbulence, turbulence), magY + Random.Range(-turbulence, turbulence), magZ + Random.Range(-turbulence, turbulence));
+            yield return new WaitForSeconds(Time.fixedDeltaTime * 100);
+            Debug.Log("Fixed delta time: " + Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Melodious Eras/Assets/Scripts/Player/PlayerMovement2D.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum FacingDirection2D { Left, Right };
+public class PlayerMovement2D : MonoBehaviour 
+{
+    public bool autoRun = true;
+    public float speed = 7f;
+    private bool moving = false;
+    public FacingDirection2D facingDirection2D = FacingDirection2D.Right;
+    public Vector3 runDirection = Vector3.right;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!autoRun)
+        {
+            speed = 0;
+            //All movement inputs in here are temporary
+            moving = false;
+            if (Input.GetKey(KeyCode.D))
+            {
+                speed = 7;
+                this.transform.position += Vector3.right * speed * Time.deltaTime;
+                if (facingDirection2D != FacingDirection2D.Right)
+                    this.transform.Rotate(Vector3.up, 180f);
+                facingDirection2D = FacingDirection2D.Right;
+                this.anim.SetFloat("Speed", 7);
+                moving = true;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                speed = 7;
+                this.transform.position -= Vector3.right * speed * Time.deltaTime;
+                if (facingDirection2D != FacingDirection2D.Left)
+                    this.transform.Rotate(Vector3.up, 180f);
+                facingDirection2D = FacingDirection2D.Left;
+                this.anim.SetFloat("Speed", 7);
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                speed = 7;
+                this.transform.position += Vector3.forward * speed * Time.deltaTime;
+                this.anim.SetFloat("Speed", speed);
+                moving = true;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                speed = 7;
+                this.transform.position -= Vector3.forward * speed * Time.deltaTime;
+                this.anim.SetFloat("Speed", speed);
+                moving = true;
+            }
+        }
+        else
+        {
+            this.transform.position += runDirection * speed * Time.deltaTime; //autorun
+        }
+	}
+}
