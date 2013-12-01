fb9bb2f056931b5ffe7bd7adbb7195b859a89203
Muhammad Naviwala <majidnaviwala@gmail.com>
2013-12-01 05:01:18 -0600

    Added some rain and lightning flash effect to island scene

---
+++ Melodious Eras/Assets/LightningFlash.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningFlash : MonoBehaviour {
+
+	IEnumerator Start ()
+	{
+		while (true)
+		{
+			light.enabled = !(light.enabled); //toggle on/off the enabled property
+			yield return new WaitForSeconds(Random.Range(0.05f, 0.5f));
+		}
+	}
+}
