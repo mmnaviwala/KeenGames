370e2cb1f499a257fd13659d6949051232efb63b
Muhammad Naviwala <majidnaviwala@gmail.com>
2013-12-03 16:42:20 -0600

    Add explosion effect

---
+++ Melodious Eras/Assets/triggerBarrelExplosion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerBarrelExplosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(startExplosion());
+	}
+
+	IEnumerator startExplosion()
+	{
+		yield return new WaitForSeconds(1.5f);
+		Destroy(this.gameObject);
+	}
+}
