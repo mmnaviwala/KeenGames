14d70cc3b78da761d3da7f2e8c9a19ac46e4acb6
Muhammad Naviwala <majidnaviwala@gmail.com>
2013-12-04 13:04:33 -0600

    Added new animated script for pause menu. Still needs a little tweaking though

---
+++ Melodious Eras/Assets/Scripts/animatedPauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animatedPauseMenu : MonoBehaviour {
+	
+	private bool gamePaused = false;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				iTween.MoveTo(GameObject.Find("gui texture bottom left").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Bottom Left Texture In"), "time", 1, "ignoretimescale", true));
+				iTween.MoveTo(GameObject.Find("gui texture bottom right").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Bottom Right Texture In"), "time", 1, "ignoretimescale", true));
+				iTween.MoveTo(GameObject.Find("gui texture left").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Left Texture In"), "time", 1, "ignoretimescale", true));
+				iTween.MoveTo(GameObject.Find("gui texture right").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Right Texture In"), "time", 1, "ignoretimescale", true));
+				iTween.MoveTo(GameObject.Find("gui texture top").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Top Texture In"), "time", 1, "ignoretimescale", true));
+			}
+			else
+			{
+				Time.timeScale = 1;
+				iTween.MoveTo(GameObject.Find("gui texture bottom left").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Bottom Left Texture Out"), "time", 1, "ignoretimescale", true));
+				iTween.MoveTo(GameObject.Find("gui texture bottom right").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Bottom Right Texture Out"), "time", 1, "ignoretimescale", true));
+				iTween.MoveTo(GameObject.Find("gui texture left").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Left Texture Out"), "time", 1, "ignoretimescale", true));
+				iTween.MoveTo(GameObject.Find("gui texture right").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Right Texture Out"), "time", 1, "ignoretimescale", true));
+				iTween.MoveTo(GameObject.Find("gui texture top").gameObject, iTween.Hash("path",iTweenPath.GetPath("Move Top Texture Out"), "time", 1, "ignoretimescale", true));			
+			}
+		}
+
+
+	}
+
+//	void OnGUI()
+//	{
+//		if (Time.timeScale == 0)
+//		{
+//			GUI.Label(new Rect(10,10,100,20), "Game Paused");
+//			Debug.Log("Should have created GUI Label");
+//		}
+//	}
+
+
+}
---
+++ Melodious Eras/Assets/Scripts/checkIfClicked.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkIfClicked : MonoBehaviour {
+
+	public string guiTextureNameShouldBe;
+
+	void OnMouseDown()
+	{
+		if(guiTexture.name == guiTextureNameShouldBe)
+		{
+			// do something here
+			Debug.Log(guiTextureNameShouldBe + " was clicked");
+		}
+	}
+}
