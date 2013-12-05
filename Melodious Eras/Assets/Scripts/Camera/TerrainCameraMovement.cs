19d5d7f408a71b1215561d16dba2b5c1aae58dd9
Muhammad Naviwala <majidnaviwala@gmail.com>
2013-12-05 11:46:51 -0600

    Added GUI buttons and music to the game menu scene

---
+++ Melodious Eras/Assets/Scripts/Camera/TerrainCameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainCameraMovement : MonoBehaviour {
+
+	public float positionX;
+	public float positionZ;
+	public float rotationY;
+	public float rotationx;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = new Vector3(transform.position.x + positionX, transform.position.y, transform.position.z + positionZ);
+		if (transform.position.x > 500f)
+			transform.rotation = new Quaternion(transform.rotation.x + rotationx, transform.rotation.y + rotationY, transform.rotation.z, transform.rotation.w);
+		else
+			transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + rotationY, transform.rotation.z, transform.rotation.w);
+	}
+}
---
+++ Melodious Eras/Assets/Scripts/Misc/checkIfClicked.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkIfClicked : MonoBehaviour {
+
+	public string guiTextureNameShouldBe;
+	GUITexture normalTexture;
+	GUITexture hoverTexture;
+
+	void OnMouseDown()
+	{
+		if(guiTexture.name == guiTextureNameShouldBe)
+		{
+			Debug.Log(guiTextureNameShouldBe + " was clicked");
+		}
+
+		if(guiTexture.name == "Gui Button Bottom")
+		{
+			Application.LoadLevel("scene_stealth_test");
+		}
+
+		if(guiTexture.name == "Gui Button Top")
+		{
+			Application.LoadLevel("Military Complex");
+		}
+	}
+
+	void OnMouseOver()
+	{
+	}
+}
