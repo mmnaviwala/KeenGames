78cef655cc41e1319becc7d7780bdb57b18c23a4
jrhart08 <josephrhart08@yahoo.com>
2014-02-01 17:46:45 -0600

    etc

---
+++ Melodious Eras/Assets/Scripts/Environment/AmbientLightTrigger.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientLightTrigger : MonoBehaviour 
+{
+	public Color ambientLight1, ambientLight2;
+	public TriggerDirection TriggerDirection1; //turns lights on
+	public TriggerDirection TriggerDirection2;//turns lights off
+	public float fadeSpeed = 2;
+	YieldInstruction endOfFrame = new WaitForEndOfFrame();
+
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Triggered");
+		if(other is CapsuleCollider && other.tag == Tags.PLAYER)
+		{
+			Vector3 relPosition = other.transform.position - this.transform.position;
+			
+			switch(TriggerDirection1)
+			{
+			case TriggerDirection.PosX: 
+				if(relPosition.x > 0)
+					Switch (true);
+				break;
+			case TriggerDirection.NegX: 
+				if(relPosition.x < 0)
+					Switch (true);
+				break;
+			case TriggerDirection.PosY: 
+				if(relPosition.y > 0)
+					Switch (true);
+				break;
+			case TriggerDirection.NegY: 
+				if(relPosition.y< 0)
+					Switch (true);
+				break;
+			case TriggerDirection.PosZ: 
+				if(relPosition.z > 0)
+					Switch (true);
+				break;
+			case TriggerDirection.NegZ: 
+				if(relPosition.z < 0)
+					Switch (true);
+				break;
+			}
+			
+			switch(TriggerDirection2)
+			{
+			case TriggerDirection.PosX: 
+				if(relPosition.x > 0)
+					Switch (false);
+				break;
+			case TriggerDirection.NegX: 
+				if(relPosition.x < 0)
+					Switch (false);
+				break;
+			case TriggerDirection.PosY: 
+				if(relPosition.y > 0)
+					Switch (false);
+				break;
+			case TriggerDirection.NegY: 
+				if(relPosition.y< 0)
+					Switch (false);
+				break;
+			case TriggerDirection.PosZ: 
+				if(relPosition.z > 0)
+					Switch (false);
+				break;
+			case TriggerDirection.NegZ: 
+				if(relPosition.z < 0)
+					Switch (false);
+				break;
+			}
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{if(other is CapsuleCollider && other.tag == Tags.PLAYER)
+		{
+			Vector3 relPosition = other.transform.position - this.transform.position;
+			
+			switch(TriggerDirection1)
+			{
+			case TriggerDirection.PosX: 
+				if(relPosition.x < 0)
+					Switch (true);
+				break;
+			case TriggerDirection.NegX: 
+				if(relPosition.x > 0)
+					Switch (true);
+				break;
+			case TriggerDirection.PosY: 
+				if(relPosition.y < 0)
+					Switch (true);
+				break;
+			case TriggerDirection.NegY: 
+				if(relPosition.y> 0)
+					Switch (true);
+				break;
+			case TriggerDirection.PosZ: 
+				if(relPosition.z < 0)
+					Switch (true);
+				break;
+			case TriggerDirection.NegZ: 
+				if(relPosition.z > 0)
+					Switch (true);
+				break;
+			}
+			
+			switch(TriggerDirection2)
+			{
+			case TriggerDirection.PosX: 
+				if(relPosition.x < 0)
+					Switch (false);
+				break;
+			case TriggerDirection.NegX: 
+				if(relPosition.x > 0)
+					Switch (false);
+				break;
+			case TriggerDirection.PosY: 
+				if(relPosition.y < 0)
+					Switch (false);
+				break;
+			case TriggerDirection.NegY: 
+				if(relPosition.y> 0)
+					Switch (false);
+				break;
+			case TriggerDirection.PosZ: 
+				if(relPosition.z < 0)
+					Switch (false);
+				break;
+			case TriggerDirection.NegZ: 
+				if(relPosition.z > 0)
+					Switch (false);
+				break;
+			}
+		}
+		
+	}
+	
+	void Switch(bool oneOr2)
+	{
+		Color goal = oneOr2 ? ambientLight1 : ambientLight2;
+		StartCoroutine(LerpColor (goal));
+	}
+	
+	void OnDrawGizmos () {
+		if (transform.parent) {
+			Gizmos.color = new Color (0.5f, 0.5f, 0.5f, 0.15f);
+			Gizmos.DrawCube (collider.bounds.center, collider.bounds.size );
+		}
+	}
+	
+	void OnDrawGizmosSelected () {
+		if (transform.parent) {
+			Gizmos.color = new Color (0.5f, 0.5f, .5f, 0.5f);
+			Gizmos.DrawCube (collider.bounds.center, collider.bounds.size );
+		}
+	}
+
+	IEnumerator LerpColor(Color goal)
+	{
+		while(Mathf.Abs(RenderSettings.ambientLight.r - RenderSettings.ambientLight.r) > .005)
+		{
+			RenderSettings.ambientLight = Color.Lerp (RenderSettings.ambientLight, goal, fadeSpeed * Time.deltaTime);
+			yield return endOfFrame;
+		}
+	}
+}
