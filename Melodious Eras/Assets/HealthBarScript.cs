6fce0608b82537fc06ebcf7f646687951b34fdf5
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-03-04 15:44:27 -0600

    Testing new health bar

---
+++ Melodious Eras/Assets/HealthBarScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarScript : MonoBehaviour {
+
+	private GUITexture healthBar;
+	private Vector3 healthScale;
+	private Rect tempPixelInset;
+	public int health = 100;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		healthBar = gameObject.guiTexture;
+		healthScale = healthBar.transform.localScale;
+		Debug.Log("Health Scale:");
+		Debug.Log(healthScale);
+		Debug.Log("Pixel Inset");
+		Debug.Log(healthBar.pixelInset);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Set the health bar's colour to proportion of the way between green and red based on the player's health.
+		healthBar.color = Color.Lerp(Color.green, Color.red, 1 - health * 0.01f);
+		
+		// Set the scale of the health bar to be proportional to the player's health.
+		// healthBar.transform.localScale = new Vector3(healthScale.x * health * 0.01f, 1, 1);
+		tempPixelInset = healthBar.pixelInset;
+		tempPixelInset.width = health;
+		healthBar.pixelInset = tempPixelInset;
+	
+	}
+}
