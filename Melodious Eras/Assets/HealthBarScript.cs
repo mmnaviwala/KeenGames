1d2bd3c1fa96a6cc927582ae1c23d526ddfe0778
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-03-04 15:46:04 -0600

    Forgot a script change in the last commit

--- Melodious Eras/Assets/HealthBarScript.cs
+++ Melodious Eras/Assets/HealthBarScript.cs
@@ -6,9 +6,8 @@ public class HealthBarScript : MonoBehaviour {
 	private GUITexture healthBar;
 	private Vector3 healthScale;
 	private Rect tempPixelInset;
-	public int health = 100;
+	public int health = 100;		// Tweak this value in the inspector
 
-	// Use this for initialization
 	void Awake () 
 	{
 		healthBar = gameObject.guiTexture;
@@ -19,14 +18,10 @@ public class HealthBarScript : MonoBehaviour {
 		Debug.Log(healthBar.pixelInset);
 	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
-		// Set the health bar's colour to proportion of the way between green and red based on the player's health.
 		healthBar.color = Color.Lerp(Color.green, Color.red, 1 - health * 0.01f);
-		
-		// Set the scale of the health bar to be proportional to the player's health.
-		// healthBar.transform.localScale = new Vector3(healthScale.x * health * 0.01f, 1, 1);
+
 		tempPixelInset = healthBar.pixelInset;
 		tempPixelInset.width = health;
 		healthBar.pixelInset = tempPixelInset;
